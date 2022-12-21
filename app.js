21efd1d425d0b9b077b2554e18b85cc53d6abaeb
Ibrahim V K <89912630+ibrhmvk@users.noreply.github.com>
2022-12-21 16:11:13 +0530

    momgodb

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://admin:admin@nodetut.jc4vjlg.mongodb.net/uselss-blog?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((error) => console.log(error));
+
 // register view engine
 app.set("view engine", "ejs");
-
-// listen for requests
-app.listen(3000);
 
 // middleware and static files
 app.use(express.static("public"));
